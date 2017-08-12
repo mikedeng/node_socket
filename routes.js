4e1def4be01e1f6fc986f385b3d2940a2a6beae1
dengming <dengming@uniqueway.com>
2017-08-12 22:42:57 +0800

    validation

--- routes.js
+++ routes.js
@@ -1,13 +1,32 @@
 'use strict';
 
-var configRoutes,
-	mongodb =	require('mongodb'),
+var loadSchema,
+	checkSchema,
+	configRoutes,
+	fsHandle    = require('fs'),
+	JSV         = require('JSV').JSV,
+	mongodb     = require('mongodb'),
 	MongoClient = mongodb.MongoClient,
-	url = 'mongodb://localhost:27017/spa',
+	url         = 'mongodb://localhost:27017/spa',
 	makeMongoId = mongodb.ObjectID,
 	opDb,
-	objTypeMap = { 'users': {} };
+	objTypeMap  = { 'users': {} },
+	validator   = JSV.createEnvironment()
 ;
+
+loadSchema = function(schema_name, schema_path){
+	fsHandle.readFile(schema_path, 'utf8', function(err, data){
+		objTypeMap[schema_name] = JSON.parse(data);
+	})
+};
+
+checkSchema = function(obj_type, obj_map, callback){
+	var
+		schema_map = objTypeMap [obj_type],
+		report_map = validator.validate(obj_map, schema_map)
+	;
+	callback( report_map.errors );
+};
 
 opDb = function(request, callback){
 	MongoClient.connect(url, function(err, db) {
@@ -43,14 +62,23 @@ configRoutes = function(app, server){
 	});
 
 	app.post('/:obj_types', function(request, response){
-		opDb(request, function(outer_error, collection){
-			var options_map = { safe: true },
-					obj_map = request.body;
+		var obj_type = request.params.obj_types,
+			obj_map = request.body;
 
-			collection.insert(obj_map, options_map, function(inner_error, result_map){
-				response.send(result_map);
-			});
-		});
+		checkSchema(obj_type, obj_map, function(error_list){
+			if( error_list.length === 0 ){
+				opDb(request, function(outer_error, collection){
+					var options_map = { safe: true },
+							obj_map = request.body;
+
+					collection.insert(obj_map, options_map, function(inner_error, result_map){
+						response.send(result_map);
+					});
+				});
+			}else{
+				response.send({ error_msg: 'Input document not valid', error_list: error_list });
+			}
+		})
 	});
 
 	app.get('/:obj_types/:id', function(request, response){
@@ -92,7 +120,7 @@ configRoutes = function(app, server){
 
 			collection.remove(
 				find_map, 
-				options_map, 
+				options_map,
 				function(inner_error, delete_count){
 					response.send({ delete_count: delete_count });
 			});
@@ -100,3 +128,13 @@ configRoutes = function(app, server){
 	});
 }
 module.exports = { configRoutes: configRoutes };
+
+(function(){
+	var schema_name, schema_path;
+	for(schema_name in objTypeMap){
+		if( objTypeMap.hasOwnProperty( schema_name )){
+			schema_path = __dirname + '/' + schema_name + '.json';
+			loadSchema(schema_name, schema_path);
+		}
+	}
+}());
